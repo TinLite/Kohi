f2a3a9151ac0b7891b0f6f0cfb1b51c59a050149
TinLite <37470201+TinLite@users.noreply.github.com>
2024-08-24 16:13:22 +0700

    backend: serving frontend dist

--- kohi-backend/src/app.module.ts
+++ kohi-backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Config
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'kohi-frontend', 'dist') // Serving build file
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
