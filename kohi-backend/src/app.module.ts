eb365d79f1d8161f3f40a04240e5296120422bc1
TinLite <37470201+TinLite@users.noreply.github.com>
2024-10-02 18:48:58 +0700

    Finish adding docker

--- kohi-backend/src/app.module.ts
+++ kohi-backend/src/app.module.ts
@@ -18,7 +18,7 @@ import { FollowsModule } from './follows/follows.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/kohi'), // MongoDB
+    MongooseModule.forRoot(process.env.MONGODB_CONNECT_URI), // MongoDB
     ConfigModule.forRoot(
       {
         isGlobal: true,
