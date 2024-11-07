dec437e94a331c726a2fea13f1b06b4deecfec93
TinLite <37470201+TinLite@users.noreply.github.com>
2024-11-07 15:41:59 +0700

    Code 8h đêm 💀💀💀

--- kohi-frontend/src/context/user-context.tsx
+++ kohi-frontend/src/context/user-context.tsx
@@ -1,6 +1,6 @@
 import { getProfile } from "@/repository/user-repository";
 import { User } from "@/types/user-type";
-import React from "react";
+import React, { useEffect } from "react";
 
 export const UserContext = React.createContext<{
     user: User | null,
@@ -12,9 +12,12 @@ export const UserContext = React.createContext<{
 
 export const UserProvider = ({ children, userData }: { children: React.ReactNode, userData?: User }) => {
     const [user, setUser] = React.useState<User | null>(userData ?? null);
-    getProfile().then(setUser).catch(() => {
-        localStorage.removeItem("backend_access_token")
-    })
+    useEffect(() => {
+        getProfile().then(setUser).catch(() => {
+            localStorage.removeItem("backend_access_token")
+        })
+    }, []);
+    console.log("UserProvider", user)
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
