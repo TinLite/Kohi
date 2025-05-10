b7d6d63fabc07c500a16089b8f27fc60d16a4e45
TinLite <37470201+TinLite@users.noreply.github.com>
2025-05-10 14:39:25 +0700

    frontend: lazyload, close when clicking out of the screen

--- kohi-frontend/src/context/image-viewer-context.tsx
+++ kohi-frontend/src/context/image-viewer-context.tsx
@@ -1,30 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 
 export const ImageViewerContext = createContext<{
     isOpen: boolean;
     setIsOpen: (isOpen: boolean) => void;
-    imageUrl: string;
-    setImageUrl: (imageUrl: string) => void;
     openImage: (imageUrl: string) => void;
     closeImage: () => void;
 }>({
     isOpen: false,
     setIsOpen: () => { },
-    imageUrl: "",
-    setImageUrl: () => { },
     openImage: () => { },
     closeImage: () => { },
 });
 
 export function ImageViewerProvider({ children }: { children: React.ReactNode }) {
     const [isOpen, setIsOpen] = useState(false);
-    const [imageUrl, setImageUrl] = useState("");
+    const ref = useRef<HTMLImageElement>(null);
 
     const openImage = (url: string) => {
-        setImageUrl(url);
-        setIsOpen(true);
+        if (ref.current) {
+            ref.current.src = url;
+            setIsOpen(true);
+        }
     };
 
     const closeImage = () => {
@@ -53,8 +51,6 @@ export function ImageViewerProvider({ children }: { children: React.ReactNode })
             value={{
                 isOpen,
                 setIsOpen,
-                imageUrl,
-                setImageUrl,
                 openImage,
                 closeImage,
             }}
@@ -64,20 +60,26 @@ export function ImageViewerProvider({ children }: { children: React.ReactNode })
                 style={{
                     opacity: isOpen ? 1 : 0,
                     pointerEvents: isOpen ? "auto" : "none",
-                }}>
+                }}
+                onClick={() => closeImage()}
+            >
                 <div className="flex-1 flex items-center justify-center overflow-hidden">
                     <img
-                        src={imageUrl}
+                        ref={ref}
                         alt=""
                         className="max-w-full max-h-full object-contain transition-[scale]"
                         style={{
                             scale: isOpen ? "1.0" : "0.5",
                         }}
+                        loading="lazy"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
                     />
 
                 </div>
                 <div className="absolute right-5 top-5">
-                    <Button size="icon" variant="ghost" onClick={closeImage}>
+                    <Button size="icon" variant="secondary" onClick={closeImage}>
                         <X />
                     </Button>
                 </div>
